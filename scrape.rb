7da41260decc461ee128ea3a81fa104b3b736ddc
kisuisou <irwinlex@yahoo.co.jp>
2022-11-12 23:10:54 +0900

    1981年~2020年までの1月の最高気温を取得してcsvに出力

--- scrape.rb
+++ scrape.rb
@@ -1,1 +1,14 @@
 require 'mechanize'
+require 'csv'
+
+URL='https://www.data.jma.go.jp/obd/stats/etrn/view/monthly_a1.php?prec_no=40&block_no=1011'
+agent=Mechanize.new
+
+CSV.open('max_temp_Jan.csv','w') do |f|
+  for year in 1981..2020 do
+    page=agent.get("#{URL}&year=#{year}")
+    max_temp=page.search('table#tablefix1 tr')[3].children[8].inner_text.match(/[0-9.]+/)
+    puts "#{year}:#{max_temp}C"
+    f << [year,max_temp]
+  end
+end
